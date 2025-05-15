fcca709bef5d1278d7dcfa560a13264125110605
Eligos Alexander <eligos.alexander@reasondigital.com>
2025-05-15 18:10:46 +0100

    add skin

--- LocalSettings.php
+++ LocalSettings.php
@@ -131,6 +131,7 @@ $wgGroupPermissions["*"]["edit"] = false;
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
 $wgDefaultSkin = "vector-2022";
+wfLoadSkin( 'Vector' );
 
 # End of automatically generated settings.
 # Add more configuration options below.
